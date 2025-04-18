3db05f938f6c3867926580f07ae04ac2b4aa35fe
g.balayan <g.balayan@vk.team>
2025-04-18 15:00:11 +0300

    add

--- http_client.py
+++ http_client.py
@@ -2,17 +2,16 @@ import aiohttp
 import logging
 import traceback
 
-# Настройка логгера
 logger = logging.getLogger("http_client")
 
-# Функция для отправки POST-запроса
+# Send to server
 async def send_post_request(url, data):
     try:
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=data) as response:
                 if response.status == 200:
                     logger.info(f"Successfully sent data to {url}")
-                    return await response.json()  # Возвращаем ответ от сервера, если нужно
+                    return await response.json()
                 else:
                     logger.error(f"Failed to send data to {url}, Status Code: {response.status}")
                     return None
