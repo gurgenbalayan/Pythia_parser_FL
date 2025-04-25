0fe6954d670f5e326b9fa351bcc671a053e1085a
g.balayan <g.balayan@vk.team>
2025-04-25 11:36:12 +0300

    add

--- app/Pythia_parser_FL.py
+++ app/Pythia_parser_FL.py
@@ -3,17 +3,14 @@ from consumers.ca_entities_consumer import handle_message
 from rabbitmq_connection import get_channel
 from utils.logger import setup_logger
 import os
-from dotenv import load_dotenv
-load_dotenv()
-logger = setup_logger("main")
 
-QUEUE_NAME = os.getenv("QUEUE_NAME")
+logger = setup_logger("main")
+QUEUE_NAME = os.getenv("QUEUE_NAME", "queue_fl_entities")
 async def main():
     channel = await get_channel()
     queue = await channel.declare_queue(QUEUE_NAME, durable=True)
     logger.info(f" [*] Waiting for messages in queue '{QUEUE_NAME}'")
     await queue.consume(handle_message)
-
     try:
         await asyncio.Event().wait()
     except KeyboardInterrupt:
